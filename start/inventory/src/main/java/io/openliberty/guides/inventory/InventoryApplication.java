843d0a6c5e2eeebb0a0f2806a1e912cafc743c8c
Gilbert Kwan <gkwan@ca.ibm.com>
2023-07-26 14:14:02 -0400

    Update InventoryApplication.java (#29)
    * Update InventoryApplication.java
    
    * Update InventoryApplication.java
    
    * Update InventoryApplication.java
    
    fix prereq-check

--- start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryApplication.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryApplication.java
@@ -1,13 +1,14 @@
+// tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2022, 2023 IBM Corporation and others.
+ * Copyright (c) 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-2.0/
  *
- * Contributors:
- *     IBM Corporation - Initial implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
+// end::copyright[]
 package io.openliberty.guides.inventory;
 
 // JAX-RS
@@ -16,5 +17,4 @@ import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/inventory")
 public class InventoryApplication extends Application {
-
 }
